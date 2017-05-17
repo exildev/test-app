727451751b3dd6051b4dacf55bc0d45aac003972
antaresgeo <ctrujillozt@gmail.com>
2017-05-17 00:58:45 -0500

    se reemplazo materialize por material-desing-web

--- angular-app/src/app/app.component.ts
+++ angular-app/src/app/app.component.ts
@@ -2,22 +2,43 @@ import { Component, OnInit } from '@angular/core';
 
 declare var jQuery: any;
 declare var $: any;
+declare var mdc: any;
 
 @Component({
-    selector: 'app-root',
+    selector: 'app-grit',
     templateUrl: './app.component.html',
     styleUrls: ['./app.component.css']
 })
 export class AppComponent implements OnInit {
 
-    title = 'app works!';
+    title = 'Grit works!';
 
     public ngOnInit() {
-        $('.button-collapse').sideNav();
+        console.log('app-grit inico');
+        mdc.autoInit();
+        this.drawerInit();
+
     }
 
     public mostrarTitulo() {
         console.log( 'click' );
-        $( '.title' ).slideToggle();
+    }
+
+    private drawerInit() {
+        const drawerel = document.querySelector('.mdc-persistent-drawer');
+        const MDCPersistentDrawer = mdc.drawer.MDCPersistentDrawer;
+        const drawer = new MDCPersistentDrawer(drawerel);
+
+        document.querySelector('.demo-menu').addEventListener('click', function() {
+            drawer.open = !drawer.open;
+        });
+
+        drawerel.addEventListener('MDCPersistentDrawer:open', function() {
+            console.log('Received MDCPersistentDrawer:open');
+        });
+
+        drawerel.addEventListener('MDCPersistentDrawer:close', function() {
+            console.log('Received MDCPersistentDrawer:close');
+        });
     }
 }
---
+++ angular-app/src/app/mdc-toolbar/mdc-toolbar.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-mdc-toolbar',
+  templateUrl: './mdc-toolbar.component.html',
+  styleUrls: ['./mdc-toolbar.component.css']
+})
+export class MdcToolbarComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
